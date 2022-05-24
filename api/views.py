4397550d96ba652ff445f808785e3694c1a32252
AtifZawadBakshi <atif.zawad4432@gmail.com>
2022-05-24 13:22:27 +0600

    24May2022

--- api/views.py
+++ api/views.py
@@ -55,11 +55,19 @@ def getNote(request, pk):
     serializer = NoteSerializer(notes, many=False)
     return Response(serializer.data)
 
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(body=data['body'])
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
 @api_view(['PUT'])
 def updateNote(request, pk):
     data = request.data
     note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance = note, data = data)
+    serializer = NoteSerializer(instance=note, data = data)
    # return Response(serializer.data)
     if serializer.is_valid():
         serializer.save()
